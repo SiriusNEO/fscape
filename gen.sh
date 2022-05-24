39a225d4c2767ca199c4623b33f52831afb2962f
SiriusNEO <1713833595@qq.com>
2022-05-25 02:25:41 +0800

    fix path write

---
+++ gen.sh
@@ -0,0 +1,22 @@
+mkdir room1
+cd room1
+echo "e" > exit
+mkdir room2
+cd room2
+echo "Our world is built with C Programming language. But I prefer Java." > NPC
+mkdir room3
+cd room3
+cd ..
+mkdir room4
+cd room4
+echo "ZZZZZZZZZZ. (sleeping)" > NPC
+cd ..
+mkdir room5
+cd room5
+cd ..
+mkdir room6
+cd room6
+echo "Listen, the secret word is: Moon" > informer
+cd ..
+cd ..
+cd ..
--- preprocess.sh
+++ preprocess.sh
@@ -2,7 +2,7 @@ FFS_MOUNT_POINT=${1}
 
 make
 rm path_cache
-echo -n $FFS_MOUNT_POINT > path_cache
+echo -n $FFS_MOUNT_POINT/ > path_cache
 ./generator 3 5
 mkdir -p $FFS_MOUNT_POINT
 ./ffs -f $FFS_MOUNT_POINT -o nonempty &
